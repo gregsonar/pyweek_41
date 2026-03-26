0f379c73ffbe322332ff1316e63949517eb00475
Gregory Dudnikof <1win1@bk.ru>
2026-03-26 16:48:14 +0300

    Working Title: INTO THE DARK

--- scenes/menu_scene.py
+++ scenes/menu_scene.py
@@ -1,4 +1,5 @@
 """Main menu scene."""
+
 from __future__ import annotations
 
 import pygame
@@ -9,7 +10,7 @@ from scenes.base_scene import BaseScene
 class MenuScene(BaseScene):
     def on_enter(self) -> None:
         self._font_title = self.game.assets.font(None, 72)
-        self._font_hint  = self.game.assets.font(None, 32)
+        self._font_hint = self.game.assets.font(None, 32)
 
     def handle_event(self, event: pygame.Event) -> None:
         if event.type == pygame.KEYDOWN:
@@ -28,8 +29,12 @@ class MenuScene(BaseScene):
         screen.fill((10, 10, 25))
 
         w, h = screen.get_size()
-        title = self._font_title.render("INTO THE DARK", True, (220, 200, 140))
-        hint  = self._font_hint.render("Press ENTER or click to begin", True, (140, 140, 160))
+        title = self._font_title.render(
+            "Working Title: INTO THE DARK", True, (220, 200, 140)
+        )
+        hint = self._font_hint.render(
+            "Press ENTER or click to begin survival", True, (140, 140, 160)
+        )
 
         screen.blit(title, title.get_rect(center=(w // 2, h // 2 - 60)))
-        screen.blit(hint,  hint.get_rect(center=(w // 2, h // 2 + 40)))
+        screen.blit(hint, hint.get_rect(center=(w // 2, h // 2 + 40)))
